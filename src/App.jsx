2eebcb989d38c799bd39626a1e9e06003c3f6f50
Gideon Idoko <iamgideonidoko@gmail.com>
2021-09-15 20:24:33 +0100

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import {Route, Switch, BrowserRouter as Router} from "react-router-dom";
 import Home from './pages/Home';
+import { Container } from '@styles/App.styled';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Container>
+          <Route exact path="/" component={Home} />
+        </Container>
       </Switch>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
-import {Container} from '@styles/Home.styled';
+import { HomeWrapper, H3, P, HomeDiv1 } from '@styles/Home.styled.js';
+import {ReactComponent as AcxLightLogo} from '@assets/icons/acxLightLogo.svg';
 
 const Home = () => {
     return (
-        <Container>
-            <h3>AlphaCX Live Chat</h3>
-        </Container>
+        <HomeWrapper>
+            <HomeDiv1>
+                <div>
+                    <AcxLightLogo/>
+                </div>
+                <H3>Welcome, we're AlphaCX!</H3>
+                <P>Here's a few quick ways you can connect with us.</P>
+            </HomeDiv1>
+            <div></div>
+        </HomeWrapper>
     )
 }
 
